4aa48d5e792f9a9fe8a372f0dd0d5801b5f26a6a
Ronit Gurjar <ronitgjcarl0ver.com>
2023-03-25 22:38:11 +0530

    adding the bird image

--- code/flappy.js
+++ code/flappy.js
@@ -4,9 +4,30 @@ let boardwidth = 360;
 let boardheight = 640;
 let context;
 
+//flappy-bird
+let birdwidth = 107;
+let birdheight = 109;
+let birdx = boardwidth/8;
+let birdy = boardheight/2
+let bird_skin;
+
+let bird={
+    x:birdx,
+    y:birdy,
+    width:birdwidth,
+    height:birdheight
+}
+
 window.onload = function(){
     board = document.getElementById("board");
     board.height = boardheight;
     board.width = boardwidth;
     context = board.getContext("2d"); // used to draw on board
+
+    //draw the bird
+    bird_skin = new Image();
+    bird_skin.src = "./pngwing_bird.png"
+    bird_skin.onload = function(){
+        context.drawImage(bird_skin,birdx,birdy,bird.width,bird.height);
+    }
 }
